32ffe0a09abecd2b67355ec6f4a4367c9225eefe
Fy64 <fy6644@gmail.com>
2025-05-07 12:26:42 -0600

    Regresar titulo a lo normal

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Agencia Canito Capitos</title>
+        <title>Agencia Elmas Capitos</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <style>
             .card-img-top {
